9ce9524f5cd9b473e5b4c5cc2eddb7f9a26a466e
SHIU <sia12amin@gmail.com>
2018-03-06 21:46:44 +0530

    added commit

--- app/src/main/java/www/newproj/com/newpro/ThirdActivity.java
+++ app/src/main/java/www/newproj/com/newpro/ThirdActivity.java
@@ -2,7 +2,7 @@ package www.newproj.com.newpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//added comment
 public class ThirdActivity extends AppCompatActivity {
 
     @Override
